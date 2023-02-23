9a06e7137e6879e18911a08856f310df0b1f90f1
YonatanYavin <73889740+YonatanYavin@users.noreply.github.com>
2023-02-23 11:19:56 +0000

    U

--- pages/Home.js
+++ pages/Home.js
@@ -7,6 +7,7 @@ const PrivatePage = ({ user }) => (
         <h1>Hello {user.username}</h1>
         <p>Secret Content</p>
         <p>Extra Testing</p>
+        <p>Extra Testing123</p>
     </div>
 );
 
